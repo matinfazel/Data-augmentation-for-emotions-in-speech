a60f4332cd2abd7a6aef566abf2b6f86c3b2554f
matinfazel <matinfazel2000@gmail.com>
2023-12-29 14:23:09 +0330

    Fid score has been added

--- Generative_Model/Data_Loader.py
+++ Generative_Model/Data_Loader.py
@@ -8,13 +8,15 @@ from PIL import Image
 from torchvision.utils import make_grid
 import matplotlib.pyplot as plt
 
-def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
+def show_tensor_images(image_tensor, step, num_images=25, size=(1, 28, 28)):
 
     image_tensor = (image_tensor + 1) / 2
     image_shifted = image_tensor
     image_unflat = image_shifted.detach().cpu().view(-1, *size)
     image_grid = make_grid(image_unflat[:num_images], nrow=5)
     plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.savefig(f"/content/drive/MyDrive/Gan_Results/Step{step}.png")  # Save the image
+
     plt.show()
 
     
